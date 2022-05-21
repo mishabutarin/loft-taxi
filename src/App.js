4e2b513ead4d2940c96c59c011724e4ea6f97552
Michael Butaryn <misha.butarin@gmail.com>
2022-05-21 13:21:27 +0300

    header moved to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
-import Map from './components/map/map'
+import Header from './components/header/header';
+import Login from './components/header/header'
+import { Profile } from './components/profile/Profile';
+import Map from './components/map/map';
+const PAGES = {
+  login: <Login/>,
+  profile: <Profile/>,
+  map: <Map/>
+}
 class App extends React.Component {
   render() {
     return <>
-    < Map/>
+    < Header/>
+    <main>{PAGES[this.state.currentPage]}</main>
     </>
   }
 }

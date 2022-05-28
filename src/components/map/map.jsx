b771c2512ccdbf96f6b29bc88ba168c43bca5b79
Michael Butaryn <misha.butarin@gmail.com>
2022-05-28 13:13:27 +0300

    header dosent work

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
-import map from '../../icons/map.png'
+import map from '../../icons/map.png';
 import Header from '../header/header';
-class Map extends React.Component{
-    constructor( props ){
-        super(props);
-        this.state = {currentPage:Map};
-        this.navigateTo = this.navigateTo.bind(this);
-    }
-    navigateTo = (page) => {
-        this.setState({ currentPage:page})
-    }
-    render (){
+function Map({ navigateTo }){
         return(
             <div className='wrapper'>
                 <div>
-                    <Header navigateTo={ this.navigateTo }/>
+                    <Header navigateTo={ navigateTo }/>
                     </div>
                <main className='map__main'>
                    <img src={ map } alt="map" />
                </main>
             </div>
-            
         );
-    }
 }
 export default Map;
